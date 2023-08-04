cef160175cf0fd91d848d8999e91a76f162f084b
SPC-Toad <sangyunkiment@gmail.com>
2023-08-04 14:57:32 -0400

    Day6_commit

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -14,15 +14,15 @@ function Navbar() {
   return (
       <div className='nav-container'>
         <ul id='section-list'>
-          <Link id='section-item' to="HOME" spy={true} smooth={true} offset={-40} duration={1000}>HOME</Link>
-          <Link id='section-item' to="PROJECT" spy={true} smooth={true} offset={0} duration={1000}>PROJECT</Link>
+          <Link id='section-item' to="PROJECT" spy={true} smooth={true} offset={20} duration={1000}>PROJECT</Link>
           <Link id='section-item' to="MILITARY" spy={true} smooth={true} offset={10} duration={1000}>MILITARY</Link>
-          <Link id='section-item' to="CONTACT" spy={true} smooth={true} offset={-50} duration={1000}>CONTACTS</Link>
+          <Link id='section-item' to="CONTACT" spy={true} smooth={true} offset={20} duration={1000}>CONTACTS</Link>
         </ul>
         <div id='time-container'>
           <div id='section-item'>{time.toLocaleTimeString()}</div>
           <div id='section-item'>{time.toLocaleDateString()}</div>
         </div>
+        <Link id='home-button' to="HOME" spy={true} smooth={true} offset={-100} duration={1000}>UP</Link>
       </div>
   )
 }
