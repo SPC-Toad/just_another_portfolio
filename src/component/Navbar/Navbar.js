1bf942af5dfe3b7dcf4f550c9033cafd7f48bd64
SPC-Toad <sangyunkiment@gmail.com>
2023-08-04 00:45:55 -0400

    Day6_commit

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -14,10 +14,10 @@ function Navbar() {
   return (
       <div className='nav-container'>
         <ul id='section-list'>
-          <Link id='section-item' to="HOME" spy={true} smooth={true} offset={0} duration={500}>HOME</Link>
-          <Link id='section-item' to="PROJECT" spy={true} smooth={true} offset={-50} duration={500}>PROJECT</Link>
-          <Link id='section-item' to="MILITARY" spy={true} smooth={true} offset={-50} duration={500}>MILITARY</Link>
-          <Link id='section-item' to="CONTACT" spy={true} smooth={true} offset={-50} duration={500}>CONTACTS</Link>
+          <Link id='section-item' to="HOME" spy={true} smooth={true} offset={-40} duration={1000}>HOME</Link>
+          <Link id='section-item' to="PROJECT" spy={true} smooth={true} offset={0} duration={1000}>PROJECT</Link>
+          <Link id='section-item' to="MILITARY" spy={true} smooth={true} offset={10} duration={1000}>MILITARY</Link>
+          <Link id='section-item' to="CONTACT" spy={true} smooth={true} offset={-50} duration={1000}>CONTACTS</Link>
         </ul>
         <div id='time-container'>
           <div id='section-item'>{time.toLocaleTimeString()}</div>
